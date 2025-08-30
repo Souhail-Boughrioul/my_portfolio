94f75afc54f93f11db7515210e9a7f91189a98b6
Souhail <souhailboughrioul@gmail.com>
2025-08-30 17:37:56 +0100

    add some change in styling to theme toggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -30,7 +30,7 @@ function ThemeToggle() {
 
   return (
     <button onClick={toggleTheme} className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "fixed max-sm:hidden top-5 right-3 z-50 p-2 rounded-full transition-colors duration-300",
         "focus:outline-hidden"
     )}>
         {isDarkMode ? (<Sun className="h-6 w-6 text-yellow-300"/>) : 
