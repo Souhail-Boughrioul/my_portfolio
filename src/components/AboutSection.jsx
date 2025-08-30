6588ab1196e0cdb6995d4243cbd05d682c0ddd4f
Souhail <souhailboughrioul@gmail.com>
2025-08-30 17:28:36 +0100

    update about me section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,6 @@ function AboutSection() {
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
               <a href="#contact" className="cosmic-button">Get In Touch</a>
 
-              <a href="#cv" target="_blank" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download CV</a>
             </div>
           </div>
 
